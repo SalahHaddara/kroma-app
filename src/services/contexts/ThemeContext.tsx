715cb581d211823b8036d7820730360a8a5c9089
Salah Haddara <Salahhaddara02@gmail.com>
2024-12-19 22:20:38 +0200

    import React

--- src/services/contexts/ThemeContext.tsx
+++ src/services/contexts/ThemeContext.tsx
@@ -1,4 +1,4 @@
-import {createContext, useState, useEffect, FC, ReactNode} from 'react';
+import React, {createContext, useState, useEffect, FC, ReactNode} from 'react';
 import {ThemeContextType} from '../../types/interfaces';
 
 export const ThemeContext = createContext<ThemeContextType>({
