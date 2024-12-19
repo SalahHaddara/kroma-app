d984c6d9e72eaba170ff75c8a5c81bcb9f88f915
Salah Haddara <Salahhaddara02@gmail.com>
2024-12-19 13:34:00 +0200

    Refactor app.jsx to app.tsx with context and routing setup

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {ThemeProvider} from './services/contexts/ThemeContext';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import Home from './pages/Home';
+// import About from './pages/About';
+// import Privacy from './pages/Privacy';
+// import Contact from './pages/Contact';
+
+const App = () => {
+    return (
+        <ThemeProvider>
+            <Router>
+                <div className="min-h-screen">
+                    <Header/>
+                    <Routes>
+                        <Route path="/" element={<Home/>}/>
+                        {/*<Route path="/about" element={<About/>}/>*/}
+                        {/*<Route path="/privacy" element={<Privacy/>}/>*/}
+                        {/*<Route path="/contact" element={<Contact/>}/>*/}
+                    </Routes>
+                    <Footer/>
+                </div>
+            </Router>
+        </ThemeProvider>
+    );
+};
+
+export default App;
