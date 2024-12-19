e07de57b321255bbaeec7e69219433a0aaee4083
Salah Haddara <Salahhaddara02@gmail.com>
2024-12-19 14:09:34 +0200

    add an interface for the Feature object

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -2,3 +2,14 @@ export interface ThemeContextType {
     theme: 'dark' | 'light';
     toggleTheme: () => void;
 }
+
+export interface Feature {
+    title: string;
+    icon: string;
+    description: string;
+    buttonText: string;
+    imageSrc: {
+        dark: string;
+        light: string;
+    };
+}
