dcbf955f0584fca4d31d8a8d1deb77bcc3452c12
Salah Haddara <Salahhaddara02@gmail.com>
2025-01-06 19:40:28 +0200

    Add Authentication response interface

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,3 +1,13 @@
 import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000/api';
+
+interface AuthResponse {
+    token: string;
+    user: {
+        _id: string;
+        fullName: string;
+        email: string;
+        avatar?: string;
+    }
+}
