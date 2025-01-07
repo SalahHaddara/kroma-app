1b9461418d645df512cc167c9e1809125f2742d0
Salah Haddara <Salahhaddara02@gmail.com>
2025-01-07 16:04:36 +0200

    Create AuthContext with initial values

--- src/services/contexts/AuthContext.tsx
+++ src/services/contexts/AuthContext.tsx
@@ -1,3 +1,6 @@
+import React, {createContext, useContext, useEffect, useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+
 interface AuthContextType {
     isAuthenticated: boolean;
     user: any;
@@ -5,4 +8,16 @@ interface AuthContextType {
     login: (token: string, userData: any) => void;
     logout: () => void;
 }
+
+const AuthContext = createContext<AuthContextType>({
+    isAuthenticated: false,
+    user: null,
+    loading: true,
+    login: () => {
+    },
+    logout: () => {
+    }
+});
+
+
 
