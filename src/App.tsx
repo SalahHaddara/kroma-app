81bdc67308e71ca4f8632dd01a8b234f4d373e8f
Salah Haddara <Salahhaddara02@gmail.com>
2025-01-07 16:27:52 +0200

    Add AuthProvider to provide authenticaiton to the app

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Footer from './components/layout/Footer';
 import Home from './pages/Home';
 import React from 'react';
 import AuthPage from "./pages/AuthPage";
+import {AuthProvider} from "@/services/contexts/AuthContext";
 
 // import About from './pages/About';
 // import Privacy from './pages/Privacy';
@@ -14,19 +15,21 @@ const App = () => {
     return (
         <ThemeProvider>
             <Router>
-                <div className="min-h-screen">
-                    <Header/>
-                    <Routes>
-                        <Route path="/" element={<Home/>}/>
-                        <Route path="/login" element={<AuthPage isLogin={true}/>}/>
-                        <Route path="/signup" element={<AuthPage isLogin={false}/>}/>
+                <AuthProvider>
+                    <div className="min-h-screen">
+                        <Header/>
+                        <Routes>
+                            <Route path="/" element={<Home/>}/>
+                            <Route path="/login" element={<AuthPage isLogin={true}/>}/>
+                            <Route path="/signup" element={<AuthPage isLogin={false}/>}/>
 
-                        {/*<Route path="/about" element={<About/>}/>*/}
-                        {/*<Route path="/privacy" element={<Privacy/>}/>*/}
-                        {/*<Route path="/contact" element={<Contact/>}/>*/}
-                    </Routes>
-                    <Footer/>
-                </div>
+                            {/*<Route path="/about" element={<About/>}/>*/}
+                            {/*<Route path="/privacy" element={<Privacy/>}/>*/}
+                            {/*<Route path="/contact" element={<Contact/>}/>*/}
+                        </Routes>
+                        <Footer/>
+                    </div>
+                </AuthProvider>
             </Router>
         </ThemeProvider>
     );
